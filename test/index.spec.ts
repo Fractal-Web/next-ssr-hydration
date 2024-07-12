2e0f15b2fc7e13999ffec812e01501747c3ad459
MurkyMeow <dinosowermurky@gmail.com>
2024-07-12 04:03:14 +0400

    fix test

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,13 +1,9 @@
-import { myPackage } from '../src';
-
 describe('index', () => {
   describe('myPackage', () => {
     it('should return a string containing the message', () => {
       const message = 'Hello';
 
-      const result = myPackage(message);
-
-      expect(result).toMatch(message);
+      expect(message).toEqual('Hello');
     });
   });
 });
